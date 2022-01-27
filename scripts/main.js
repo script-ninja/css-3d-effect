67fda333649f8e487709b94ed0b880bc870dd302
script-ninja <61618647+script-ninja@users.noreply.github.com>
2022-01-27 05:33:56 +0500

    Добавлен слайдер

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
+	const slider = new Swiper('.carousel', {
+		loop: true,
+		slidesPerView: 3,
+		speed: 1800,
+		centeredSlides: true,
+	});
+
 	document.querySelector('video').playbackRate = 2;
 });
